ded236f8f3ae4b77d00a8e4b4b53e8441296a130
aaronfox <aaron.m.fox3@gmail.com>
2021-03-27 22:13:44 -0400

    provide initial workings of allowing user to interact with grid

---
+++ game_of_life_v1/public/js/game.js
@@ -0,0 +1,234 @@
+var config = {
+    type: Phaser.AUTO,
+    parent: '#canvas',
+    width: 800,
+    height: 600,
+    backgroundColor: '#f0ebeb',
+    scene: {
+        preload: preload,
+        create: create,
+        update: update
+    }
+};
+
+var game = new Phaser.Game(config);
+var graphics;
+
+let hspace = 10;
+let size = {
+    x: 50,
+    y: 50
+}
+
+function preload() {
+}
+
+function create() {
+    graphics = this.add.graphics({
+        lineStyle: {
+            width: 1,
+            color: 0xffffff,
+            alpha: 1
+        }
+    });
+
+    // Draw initial grid
+    for (let ix = 0; ix < size.x; ix++) {
+        for (let iy = 0; iy < size.y; iy++) {
+            // if (iy % 3 == 0) {
+            //     var color = 0xffff00;
+            //     var alpha = 0.5 + ((4 / 10) * 0.5);
+            //     graphics.fillStyle(color, alpha);
+            // } else {
+            //     var color = 0xAAFF00;
+            //     var alpha = 0.5 + ((4 / 10) * 0.5);
+
+            //     graphics.fillStyle(color, alpha);
+            // }
+            graphics.strokeRect(ix * hspace, iy * hspace, hspace, hspace);
+            // graphics.fillRect(ix * hspace, iy * hspace, hspace, hspace);
+        }
+    }
+
+    // Allows for player to move camera by dragging mouse
+    // Note: probably won't allow for this as this makes getting x and y positions of
+    // mouse inconsistent
+    // this.input.on('pointermove', (pointer) => {
+    //     if (pointer.isDown) {
+    //         this.cameras.main.scrollX -= (pointer.position.x - pointer.prevPosition.x) * 1.5;
+    //         this.cameras.main.scrollY -= (pointer.position.y - pointer.prevPosition.y) * 1.5;
+    //     }
+    // })
+
+    this.input.on('pointerdown', (pointer) => {
+        if (pointer.isDown) {
+            var color = 0x4287f5;
+            var alpha = 0.75;
+            graphics.fillStyle(color, alpha);
+            // Round position to next greatest hspace
+            let x = Math.floor(pointer.position.x / hspace) * hspace;
+            let y = Math.floor(pointer.position.y / hspace) * hspace;
+            if (x < size.x * hspace && y < size.y * hspace) {
+                graphics.strokeRect(x, y, hspace, hspace);
+                graphics.fillRect(x, y, hspace, hspace);
+            }
+            console.log('pointer.position.x == ' + pointer.position.x);
+            console.log('pointer.position.y == ' + pointer.position.y);
+            console.log('in')
+        }
+    })
+
+}
+
+function update() {
+    // Detect mouse click input
+    if (game.input.mousePointer.isDown) {
+        // graphics.fillStyle(0xAAFF00, alpha);
+        var color = 0x4287f5;
+        var alpha = 0.75;
+        graphics.fillStyle(color, alpha);
+        graphics.strokeRect(game.input.x * hspace, game.input.y * hspace, hspace, hspace);
+        graphics.fillRect(game.input.x * hspace, game.input.y * hspace, hspace, hspace);
+        // console.log('out')
+    }
+}
+
+// OLD GAME CODE
+// var config = {
+//     type: Phaser.AUTO,
+//     parent: 'phaser-example',
+//     width: 800,
+//     height: 600,
+//     physics: {
+//         default: 'arcade',
+//         arcade: { 
+//             debug: false,
+//             gravity: { y: 0 }
+//         }
+//     },
+//     scene: {
+//         preload: preload,
+//         create: create,
+//         update: update
+//     }
+// };
+
+// var game = new Phaser.Game(config);
+
+// function preload() { 
+//     this.load.image('ship', 'assets/spaceShips_001.png'); 
+//     this.load.image('otherPlayer', 'assets/enemyBlack5.png');
+//     this.load.image('star', 'assets/star_gold.png');
+// }
+
+// function create() {
+//     var self = this;
+//     this.socket = io();
+//     this.otherPlayers = this.physics.add.group();
+//     this.socket.on('currentPlayers', function (players) {
+//         Object.keys(players).forEach(function (id) {
+//             if (players[id].playerId === self.socket.id) {
+//                 addPlayer(self, players[id]);
+//             } else {
+//                 addOtherPlayers(self, players[id]);
+//             }
+//         });
+//     });
+//     this.socket.on('newPlayer', function (playerInfo) {
+//         addOtherPlayers(self, playerInfo);
+//     });
+//     this.socket.on('disconnected', function (playerId) {
+//         self.otherPlayers.getChildren().forEach(function (otherPlayer) {
+//             if (playerId === otherPlayer.playerId) {
+//                 otherPlayer.destroy();
+//             }
+//         });
+//     });
+
+//     this.cursors = this.input.keyboard.createCursorKeys();
+
+//     this.socket.on('playerMoved', function (playerInfo) {
+//         self.otherPlayers.getChildren().forEach(function (otherPlayer) {
+//             if (playerInfo.playerId === otherPlayer.playerId) {
+//                 otherPlayer.setRotation(playerInfo.rotation);
+//                 otherPlayer.setPosition(playerInfo.x, playerInfo.y);
+//             }
+//         });
+//     });
+
+//     this.blueScoreText = this.add.text(16, 16, '', { fontSize: '32px', fill: '#0000FF' });
+//     this.redScoreText = this.add.text(584, 16, '', { fontSize: '32px', fill: '#FF0000' });
+
+//     this.socket.on('scoreUpdate', function (scores) {
+//         self.blueScoreText.setText('Blue: ' + scores.blue);
+//         self.redScoreText.setText('Red: ' + scores.red);
+//     });
+
+//     this.socket.on('starLocation', function (starLocation) {
+//         if (self.star) self.star.destroy();
+//         self.star = self.physics.add.image(starLocation.x, starLocation.y, 'star');
+//         self.physics.add.overlap(self.ship, self.star, function () {
+//             this.socket.emit('starCollected');
+//         }, null, self);
+//     });
+// }
+
+// function update() { 
+//     if (this.ship) {
+//         if (this.cursors.left.isDown) {
+//             this.ship.setAngularVelocity(-150);
+//         } else if (this.cursors.right.isDown) {
+//             this.ship.setAngularVelocity(150);
+//         } else {
+//             this.ship.setAngularVelocity(0);
+//         }
+
+//         if (this.cursors.up.isDown) {
+//             this.physics.velocityFromRotation(this.ship.rotation + 1.5, 100, this.ship.body.acceleration);
+//         } else {
+//             this.ship.setAcceleration(0);
+//         }
+
+//         this.physics.world.wrap(this.ship, 5);
+
+//         // emit player movement
+//         var x = this.ship.x;
+//         var y = this.ship.y;
+//         var r = this.ship.rotation;
+//         if (this.ship.oldPosition && (x !== this.ship.oldPosition.x || y !== this.ship.oldPosition.y || r !== this.ship.oldPosition.rotation)) {
+//             this.socket.emit('playerMovement', { x: this.ship.x, y: this.ship.y, rotation: this.ship.rotation });
+//         }
+
+//         // save old position data
+//         this.ship.oldPosition = {
+//             x: this.ship.x,
+//             y: this.ship.y,
+//             rotation: this.ship.rotation
+//         };
+//     }
+
+
+// }
+
+// function addPlayer(self, playerInfo) {
+//     self.ship = self.physics.add.image(playerInfo.x, playerInfo.y, 'ship').setOrigin(0.5, 0.5).setDisplaySize(53, 40);
+//     if (playerInfo.team === 'blue') {
+//         self.ship.setTint(0x0000ff);
+//     } else {
+//         self.ship.setTint(0xff0000);
+//     }
+//     self.ship.setDrag(100);
+//     self.ship.setAngularDrag(100);
+//     self.ship.setMaxVelocity(200);
+// }
+
+// function addOtherPlayers(self, playerInfo) {
+//     const otherPlayer = self.add.sprite(playerInfo.x, playerInfo.y, 'otherPlayer').setOrigin(0.5, 0.5).setDisplaySize(53, 40);
+//     if (playerInfo.team === 'blue') {
+//         otherPlayer.setTint(0x0000ff);
+//     } else {
+//         otherPlayer.setTint(0xff0000);
+//     }
+//     otherPlayer.playerId = playerInfo.playerId;
+//     self.otherPlayers.add(otherPlayer);
+// }
---
+++ game_of_life_v1/server.js
@@ -0,0 +1,192 @@
+var express = require('express'),
+    app = express()//,
+// bodyParser = require('body-parser'),
+// methodOverride = require('method-override')
+
+var express = require('express');
+var app = express();
+
+app.set('port', (process.env.PORT || 8081));
+
+app.use(express.static(__dirname + '/public'));
+
+// ======================================= BEGIN Multi game logic =======================================
+// var server = require('http').Server(app);
+// // NOTE: changed this from original
+// var io = require('socket.io')(server);
+
+var server = app.listen(app.get('port'));
+var io = require('socket.io')(server);
+
+// Keep track of all players in game with players
+var players = {};
+
+// star variable keeps track of position of star collectibles
+var star = {
+    x: Math.floor(Math.random() * 700) + 50,
+    y: Math.floor(Math.random() * 500) + 50
+};
+// scores variable keeps track of both team's score
+var scores = {
+    blue: 0,
+    red: 0
+};
+
+io.on('connection', function (socket) {
+    console.log('a user connected');
+    // create a new player and add it to our players object
+    players[socket.id] = {
+        rotation: 0,
+        x: Math.floor(Math.random() * 700) + 50,
+        y: Math.floor(Math.random() * 500) + 50,
+        playerId: socket.id,
+        team: (Math.floor(Math.random() * 2) == 0) ? 'red' : 'blue'
+    };
+    // send the players object to the new player
+    socket.emit('currentPlayers', players);
+
+    // send the star object to the new player
+    socket.emit('starLocation', star);
+
+    // send the current scores
+    socket.emit('scoreUpdate', scores);
+
+    // update all other players of the new player
+    socket.broadcast.emit('newPlayer', players[socket.id]);
+
+    socket.on('disconnect', function () {
+        console.log('user disconnected');
+        // remove this player from our players object
+        delete players[socket.id];
+        // Check if this is last player
+        if (players.length == undefined) {
+            console.log('last!!!');
+            scores.red = 0;
+            scores.blue = 0;
+            star.x = Math.floor(Math.random() * 700) + 50;
+            star.y = Math.floor(Math.random() * 500) + 50;
+            io.emit('starLocation', star);
+            io.emit('scoreUpdate', scores);
+        }
+        // emit a message to all players to remove this player
+        // NOTE: changed this from original
+        io.emit('disconnected', socket.id);
+    });
+
+    // when a player moves, update the player data
+    socket.on('playerMovement', function (movementData) {
+        players[socket.id].x = movementData.x;
+        players[socket.id].y = movementData.y;
+        players[socket.id].rotation = movementData.rotation;
+        // emit a message to all players about the player that moved
+        socket.broadcast.emit('playerMoved', players[socket.id]);
+    });
+
+    socket.on('starCollected', function () {
+        console.log('in starCollected')
+
+        if (players[socket.id].team === 'red') {
+            scores.red += 10;
+        } else {
+            scores.blue += 10;
+        }
+        star.x = Math.floor(Math.random() * 700) + 50;
+        star.y = Math.floor(Math.random() * 500) + 50;
+        io.emit('starLocation', star);
+        io.emit('scoreUpdate', scores);
+    });
+});
+
+app.get('/multi_game', function (req, res) {
+    // res.render('pages/multi_game');
+    res.sendFile(__dirname + '/views/pages/multi_game.html');
+    // res.sendFile('pages/multi_game.html');
+
+});
+
+
+// ======================================= END Multi game logic =======================================
+
+// OLD CODE 
+// var express = require('express');
+// var app = express();
+// var server = require('http').Server(app);
+// // NOTE: changed this from original
+// var io = require('socket.io')(server);
+
+// // Keep track of all players in game with players
+// var players = {};
+
+// // star variable keeps track of position of star collectibles
+// var star = {
+//     x: Math.floor(Math.random() * 700) + 50,
+//     y: Math.floor(Math.random() * 500) + 50
+// };
+// // scores variable keeps track of both team's score
+// var scores = {
+//     blue: 0,
+//     red: 0
+// };
+
+// app.use(express.static(__dirname + '/public'));
+
+// app.get('/', function (req, res) {
+//     res.sendFile(__dirname + '/index.html');
+// });
+
+// io.on('connection', function (socket) {
+//     console.log('a user connected');
+//     // create a new player and add it to our players object
+//     players[socket.id] = {
+//         rotation: 0,
+//         x: Math.floor(Math.random() * 700) + 50,
+//         y: Math.floor(Math.random() * 500) + 50,
+//         playerId: socket.id,
+//         team: (Math.floor(Math.random() * 2) == 0) ? 'red' : 'blue'
+//     };
+//     // send the players object to the new player
+//     socket.emit('currentPlayers', players);
+
+//     // send the star object to the new player
+//     socket.emit('starLocation', star);
+
+//     // send the current scores
+//     socket.emit('scoreUpdate', scores);
+
+//     // update all other players of the new player
+//     socket.broadcast.emit('newPlayer', players[socket.id]);
+
+//     socket.on('disconnect', function () {
+//         console.log('user disconnected');
+//         // remove this player from our players object
+//         delete players[socket.id];
+//         // emit a message to all players to remove this player
+//         // NOTE: changed this from original
+//         io.emit('disconnected', socket.id);
+//     });
+
+//     // when a player moves, update the player data
+//     socket.on('playerMovement', function (movementData) {
+//         players[socket.id].x = movementData.x;
+//         players[socket.id].y = movementData.y;
+//         players[socket.id].rotation = movementData.rotation;
+//         // emit a message to all players about the player that moved
+//         socket.broadcast.emit('playerMoved', players[socket.id]);
+//     });
+
+//     socket.on('starCollected', function () {
+//         if (players[socket.id].team === 'red') {
+//             scores.red += 10;
+//         } else {
+//             scores.blue += 10;
+//         }
+//         star.x = Math.floor(Math.random() * 700) + 50;
+//         star.y = Math.floor(Math.random() * 500) + 50;
+//         io.emit('starLocation', star);
+//         io.emit('scoreUpdate', scores);
+//     });
+// });
+
+// server.listen(8081, function () {
+//     console.log(`Listening on ${server.address().port}`);
+// });
